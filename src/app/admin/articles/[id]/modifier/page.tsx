e88c17d75e26b8369beecb18ae937df170d561c3
Billy-Craweford <oumaroubilly6@gmail.com>
2026-08-25 22:22:51 +0200

    soucis import

--- src/app/admin/articles/[id]/modifier/page.tsx
+++ src/app/admin/articles/[id]/modifier/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import axios from "../../../../../lib/axios";
+import axios from "@/../lib/axios";
 import RequireAuth from "@/components/RequireAuth";
 
 type Categorie = {
